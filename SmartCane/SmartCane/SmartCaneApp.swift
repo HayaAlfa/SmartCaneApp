65762e677b08f8814a2f64a9a89a2061b1fbd0b8
Haya Alfakieh <halfakieh2020@fau.edu>
2025-08-29 14:53:47 -0400

    first commit

---
+++ SmartCane/SmartCane/SmartCaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCaneApp.swift
+//  SmartCane
+//
+//  Created by Haya Alfakieh on 8/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
